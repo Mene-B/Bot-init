9f9a18315fe40c5711972a213eba0c84426857c9
Mene-B <82470140+Mene-B@users.noreply.github.com>
2023-08-01 01:14:12 +0200

    Update command.js

--- slash-commands/command.js
+++ slash-commands/command.js
@@ -1,12 +1,32 @@
-const {SlashCommandBuilder} = require("discord.js");
-const {clientPG} = require("../database.js");
+const Discord = require("discord.js");
+const config= require("./config.json");
+const client = new Discord.Client({
+    intents: [
+        Discord.GatewayIntentBits.Guilds,
+        Discord.GatewayIntentBits.GuildMembers,
+        Discord.GatewayIntentBits.GuildMessages,
+        Discord.GatewayIntentBits.MessageContent,
+        Discord.GatewayIntentBits.GuildMessageReactions
+    ]
+});
+const fs = require("fs");
 
-module.exports = {
-  data: new SlashCommandBuilder()
-  .setName("command")
-  .setDescription("Some description"),
-  run: function(interaction){
-    console.log(interaction.commandName);
-  }
+client.login(config.token);
 
-}
+client.on("ready",()=>{
+  console.log("Bot ready to work hard !");
+});
+
+const commands = fs.readdirSync("./slash-commands").map((file)=>{
+    const data = require("./slash-commands/"+file);
+    return {name : data.data.name, run: data.run };
+});
+
+client.on("interactionCreate",(interaction)=>{
+    if(interaction.isCommand()){
+        const command = commands.find((cmd)=>{
+            return cmd.name === interaction.commandName;
+        })
+        return command.run(interaction);
+    }
+})
